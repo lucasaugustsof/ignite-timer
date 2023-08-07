2d16bbfff03145eda3e4c15ebf0ec721e904888d
Lucas Augusto <lucasaugustscode@gmail.com>
2023-08-07 18:29:15 -0300

    feat: construction of the history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export const History: React.FC = () => {
   return (
-    <Container>
+    <HistoryContainer>
       <h1>History</h1>
-    </Container>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
